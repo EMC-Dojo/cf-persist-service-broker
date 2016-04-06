362deb8856b084f963f750437e5db1e9bf54175b
Xuebin He <xuebin.he@emc.com>
2016-04-06 12:04:25 -0400

    Init repo
    [#117014223](https://www.pivotaltracker.com/story/show/117014223)
    
    Signed-off-by: Anupam Saini <anupam.s.saini@gmail.com>

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cf-persist-service-broker
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"io"
+)
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
+
+func main() {
+	http.HandleFunc("/hello", HelloServer)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
