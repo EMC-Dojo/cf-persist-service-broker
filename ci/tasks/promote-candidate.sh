2b08336e62e128f925a3c23cd3cb80353f01ce40
Joshua Aresty <joshua.aresty@emc.com>
2016-04-28 14:38:24 -0400

    Fixing an issue in promote-candidate script:
    - It appears that the version file was not being found - fixed.

--- ci/tasks/promote-candidate.sh
+++ ci/tasks/promote-candidate.sh
@@ -6,14 +6,15 @@ source cf-persist-service-broker/ci/tasks/util.sh
 
 # Creates an integer version number from the semantic version format
 # May be changed when we decide to fully use semantic versions for releases
-export integer_version=`cut -d "." -f1 version-semver/number`
+export integer_version=`cut -d "." -f1 version-semver/version`
 cp -r cf-persist-service-broker promote/cf-persist-service-broker
-echo ${integer_version} > promote/number
+echo ${integer_version} > promote/integer_version
 
 pushd promote/cf-persist-service-broker/
   echo ${tag_message} >> release_log.txt
   git add release_log.txt
   git config --global user.email emccmd-eng@emc.com
   git config --global user.name EMCCMD-CI
-  git commit -m ":airplane: New final release v ${integer_version}" -m "[ci skip]"
+  export annotate_message=":airplane: New final release v${integer_version}"
+  git commit -m ${annotate_message} -m "[ci skip]"
 popd
