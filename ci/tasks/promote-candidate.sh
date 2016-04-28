030727fbe1007c926a12890887141993ea849693
Joshua Aresty <joshua.aresty@emc.com>
2016-04-28 15:01:12 -0400

    Changed version-semver/version to version-semver/number
    Signed-off-by: Megan Murawski <megan.hyland@emc.com>

--- ci/tasks/promote-candidate.sh
+++ ci/tasks/promote-candidate.sh
@@ -6,7 +6,7 @@ source cf-persist-service-broker/ci/tasks/util.sh
 
 # Creates an integer version number from the semantic version format
 # May be changed when we decide to fully use semantic versions for releases
-export integer_version=`cut -d "." -f1 version-semver/version`
+export integer_version=`cut -d "." -f1 version-semver/number`
 cp -r cf-persist-service-broker promote/cf-persist-service-broker
 echo ${integer_version} > promote/integer_version
 
