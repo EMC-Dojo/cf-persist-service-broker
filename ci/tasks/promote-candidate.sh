38cf014860bbdfaa9eaee54a79ed39e69245eeb2
Xuebin He <xuebin.he@emc.com>
2016-04-28 14:22:57 -0400

    Needed to add release_log.txt before committing.
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- ci/tasks/promote-candidate.sh
+++ ci/tasks/promote-candidate.sh
@@ -8,10 +8,11 @@ source cf-persist-service-broker/ci/tasks/util.sh
 # May be changed when we decide to fully use semantic versions for releases
 export integer_version=`cut -d "." -f1 version-semver/version`
 cp -r cf-persist-service-broker promote/cf-persist-service-broker
-
 echo ${integer_version} > promote/number
-echo ${tag_message} >> promote/cf-persist-service-broker/release_log.txt
+
 pushd promote/cf-persist-service-broker/
+  echo ${tag_message} >> release_log.txt
+  git add release_log.txt
   git config --global user.email emccmd-eng@emc.com
   git config --global user.name EMCCMD-CI
   git commit -m ":airplane: New final release v ${integer_version}" -m "[ci skip]"
