7336384cbd7f9e0aa344a4829a73f78a77ee4a2a
Peter Blum <peter.blum@emc.com>
2016-06-29 18:13:21 -0400

    Few Updates to Concourse for new changes to SB

--- ci/tasks/integration.sh
+++ ci/tasks/integration.sh
@@ -4,21 +4,15 @@ set -e -x
 
 source cf-persist-service-broker/ci/tasks/util.sh
 
-check_param BROKER_USERNAME
+check_param TEST_INSTANCE_ID
+check_param PARSED_INSTANCE_ID
+check_param TEST_SIZE
+check_param STORAGE_POOL_NAME
 check_param BROKER_PASSWORD
+check_param BROKER_USERNAME
+check_param BROKER_PORT
 check_param LIBSTORAGE_URI
-check_param LIBSTORAGE_STORAGE_DRIVER
-check_param SCALEIO_ENDPOINT
-check_param SCALEIO_USERNAME
-check_param SCALEIO_PASSWORD
-check_param SCALEIO_SYSTEM_ID
-check_param SCALEIO_PROTECTION_DOMAIN_ID
-check_param SCALEIO_PROTECTION_DOMAIN_NAME
-check_param SCALEIO_STORAGE_POOL_NAME
-check_param SCALEIO_THIN_OR_THICK
-check_param SCALEIO_VERSION
-check_param SCALEIO_INSECURE
-check_param SCALEIO_USE_CERTS
+check_param LIBSTORAGE_DRIVER_TYPE
 
 export GOPATH=$PWD/gocode
 export PATH=$PATH:$GOPATH/bin
