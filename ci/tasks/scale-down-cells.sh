3ae38662946d01fc0304290d57d83dbc8a4754c6
Xuebin He <xuebin.he@emc.com>
2016-08-02 15:45:49 -0400

    Reorganize variables in CI
    [#126779523](https://www.pivotaltracker.com/story/show/126779523)

--- ci/tasks/scale-down-cells.sh
+++ ci/tasks/scale-down-cells.sh
@@ -3,8 +3,8 @@
 set -e -x
 source cf-persist-service-broker/ci/tasks/util.sh
 
-check_param BOSH_DIR
-check_param BOSH_PASS
+check_param BOSH_DIRECTOR_PUBLIC_IP
+check_param BOSH_PASSWORD
 check_param BOSH_USER
 check_param DIEGO_DEPLOYMENT_NAME
 
@@ -12,8 +12,8 @@ check_param DIEGO_DEPLOYMENT_NAME
 gem install bosh_cli --no-ri --no-rdoc
 
 #Setup BOSH CLI for us
-bosh -n target ${BOSH_DIR}
-bosh -n login ${BOSH_USER} ${BOSH_PASS}
+bosh -n target ${BOSH_DIRECTOR_PUBLIC_IP}
+bosh -n login ${BOSH_USER} ${BOSH_PASSWORD}
 
 manifest="$(bosh download manifest ${DIEGO_DEPLOYMENT_NAME} | perl -0pe 's/(- instances: 2\n  name: CI_cell_z1)(.+?)(- instances:)/- instances:/sg')"
 echo "${manifest}" > manifest.yml
