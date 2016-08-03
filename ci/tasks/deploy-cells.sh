10d4beeab926d94034a7fa3a9dc8ec888da3d351
Xuebin He <xuebin.he@emc.com>
2016-08-03 09:26:37 -0400

    Test2

--- ci/tasks/deploy-cells.sh
+++ ci/tasks/deploy-cells.sh
@@ -9,7 +9,6 @@ check_param BOSH_USER
 check_param DIEGO_CELL_IPS
 check_param DIEGO_DEPLOYMENT_NAME
 check_param SCALEIO_MDM_IPS
-
 #install bosh cli (should add to docker image eventually...)
 gem install bosh_cli --no-ri --no-rdoc
 
@@ -17,10 +16,14 @@ gem install bosh_cli --no-ri --no-rdoc
 bosh -n target ${BOSH_DIRECTOR_PUBLIC_IP}
 bosh -n login ${BOSH_USER} ${BOSH_PASSWORD}
 
-manifest="$(bosh download manifest ${DIEGO_DEPLOYMENT_NAME} | sed -e $'s/jobs:/jobs:\\\n- instances: 2\\\n  name: CI_cell_z1\\\n  networks:\\\n  - name: private\\\n    static_ips: ['"${DIEGO_CELL_IPS}"$']\\\n  properties:\\\n    scaleio:\\\n      mdm:\\\n        ips: ['"${SCALEIO_MDM_IPS}"$']\\\n    diego:\\\n      rep:\\\n        zone: z1\\\n    metron_agent:\\\n      zone: z1\\\n  vm_type: x-large\\\n  stemcell: trusty-3215\\\n  azs:\\\n  - z1\\\n  templates:\\\n  - name: consul_agent\\\n    release: cf\\\n  - name: rep\\\n    release: diego-release\\\n  - name: garden\\\n    release: garden-linux\\\n  - name: cflinuxfs2-rootfs-setup\\\n    release: cflinuxfs2-rootfs\\\n  - name: metron_agent\\\n    release: cf\\\n  - name: rexray_service\\\n    release: rexray-bosh-release\\\n  - name: setup_sdc\\\n    release: scaleio-sdc-bosh-release\\\n  update:\\\n    max_in_flight: 1\\\n    serial: false/g')"
+manifest="$(bosh download manifest ${DIEGO_DEPLOYMENT_NAME} | perl -0pe 's/(- instances: 2\n  name: CI_cell_z1)(.+?)(- instances:)/- instances:/sg')"
 echo "${manifest}" > manifest.yml
 
 bosh deployment manifest.yml
+bosh -n deploy
+
+manifest="$(bosh download manifest ${DIEGO_DEPLOYMENT_NAME} | sed -e $'s/jobs:/jobs:\\\n- instances: 2\\\n  name: CI_cell_z1\\\n  networks:\\\n  - name: private\\\n    static_ips: ['"${DIEGO_CELL_IPS}"$']\\\n  properties:\\\n    scaleio:\\\n      mdm:\\\n        ips: ['"${SCALEIO_MDM_IPS}"$']\\\n    diego:\\\n      rep:\\\n        zone: z1\\\n    metron_agent:\\\n      zone: z1\\\n  vm_type: x-large\\\n  stemcell: trusty-3215\\\n  azs:\\\n  - z1\\\n  templates:\\\n  - name: consul_agent\\\n    release: cf\\\n  - name: rep\\\n    release: diego-release\\\n  - name: garden\\\n    release: garden-linux\\\n  - name: cflinuxfs2-rootfs-setup\\\n    release: cflinuxfs2-rootfs\\\n  - name: metron_agent\\\n    release: cf\\\n  - name: rexray_service\\\n    release: rexray-bosh-release\\\n  - name: setup_sdc\\\n    release: scaleio-sdc-bosh-release\\\n  update:\\\n    max_in_flight: 1\\\n    serial: false/g')"
+echo "${manifest}" > manifest.yml
 bosh -n deploy
 
 echo "Diego has now evolved! RAWRRRR~"
