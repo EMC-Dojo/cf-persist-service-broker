d8e054773df8af0b2ff0d514cc40e03dfcd62e36
Xuebin He <xuebin.he@emc.com>
2016-08-09 10:00:23 -0400

    TEST1

--- ci/tasks/promote-candidate.sh
+++ ci/tasks/promote-candidate.sh
@@ -6,11 +6,12 @@ source cf-persist-service-broker/ci/tasks/util.sh
 check_param GITHUB_USER
 check_param GITHUB_EMAIL
 check_param REPO_NAME
+check_param VERSION_FILE
 
 # Creates an integer version number from the semantic version format
 # May be changed when we decide to fully use semantic versions for releases
-export integer_version=`cut -d "." -f1 version-semver/number`
-cp -r $REPO_NAME promote/${REPO_NAME}
+export integer_version=`cut -d "." -f1 ${VERSION_FILE}`
+cp -r ${REPO_NAME} promote/${REPO_NAME}
 echo ${integer_version} > promote/integer_version
 
 pushd promote/${REPO_NAME}
