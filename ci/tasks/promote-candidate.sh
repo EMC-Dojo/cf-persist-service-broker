3cc568e168c7c2d8facd054d6855c5f2bb47d3ec
Xuebin He <xuebin.he@emc.com>
2016-08-09 11:05:29 -0400

    Fix version is not showing in change log

--- ci/tasks/promote-candidate.sh
+++ ci/tasks/promote-candidate.sh
@@ -6,11 +6,12 @@ source cf-persist-service-broker/ci/tasks/util.sh
 check_param GITHUB_USER
 check_param GITHUB_EMAIL
 check_param REPO_NAME
+check_param VERSION_FILE
 
 # Creates an integer version number from the semantic version format
 # May be changed when we decide to fully use semantic versions for releases
-export integer_version=`cut -d "." -f1 version-semver/number`
-cp -r $REPO_NAME promote/${REPO_NAME}
+export integer_version=`cut -d "." -f1 ${VERSION_FILE}`
+cp -r ${REPO_NAME} promote/${REPO_NAME}
 echo ${integer_version} > promote/integer_version
 
 pushd promote/${REPO_NAME}
@@ -18,12 +19,9 @@ pushd promote/${REPO_NAME}
   git config --global user.name ${GITHUB_USER}
   git config --global push.default simple
 
-  export annotate_message=":airplane: New final release v${integer_version}"
-  export log_message="`git log -1 --pretty=oneline`"
-
   echo "## v${integer_version}" >> CHANGELOG.md
-  echo "v${log_message}" >> CHANGELOG.md
-
+  echo `git log -1 --abbrev-commit --pretty=oneline` >> CHANGELOG.md
   git add CHANGELOG.md
-  git commit -m "[ci skip] ${annotate_message}"
+
+  git commit -m ":airplane: New final release v${integer_version}" -m "[ci skip]"
 popd
