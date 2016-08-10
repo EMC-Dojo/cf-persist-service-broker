98d6f12f4b2708024fcce14b2010625802abf372
Xuebin He <xuebin.he@emc.com>
2016-08-10 19:01:04 -0400

    Fix cell deployment doesn't get right version of releases

--- ci/tasks/deploy-cells.sh
+++ ci/tasks/deploy-cells.sh
@@ -35,9 +35,9 @@ pushd scaleio-boshrelease
 popd
 
 manifest="$(bosh download manifest ${DIEGO_DEPLOYMENT_NAME} | sed -e $'s/jobs:/jobs:\\\n- instances: 2\\\n  name: CI_cell_z1\\\n  networks:\\\n  - name: private\\\n    static_ips: ['"${DIEGO_CELL_IPS}"$']\\\n  properties:\\\n    scaleio:\\\n      mdm:\\\n        ips: ['"${SCALEIO_MDM_IPS}"$']\\\n    diego:\\\n      rep:\\\n        zone: z1\\\n    metron_agent:\\\n      zone: z1\\\n  vm_type: x-large\\\n  stemcell: trusty-3215\\\n  azs:\\\n  - z1\\\n  templates:\\\n  - name: consul_agent\\\n    release: cf\\\n  - name: rep\\\n    release: diego-release\\\n  - name: garden\\\n    release: garden-linux\\\n  - name: cflinuxfs2-rootfs-setup\\\n    release: cflinuxfs2-rootfs\\\n  - name: metron_agent\\\n    release: cf\\\n  - name: rexray_service\\\n    release: ci-rexray-boshrelease\\\n  - name: setup_sdc\\\n    release: ci-scaleio-boshrelease\\\n  update:\\\n    max_in_flight: 1\\\n    serial: false/g')"
-echo "${manifest}" | perl -0pe "s/- name: ci-rexray-boshrelease\n  version: [0-9a-z\+\.]+/- name: ci-rexray-boshrelease\n  version: ${rexray_release_version}/sg" > manifest.yml
+echo "${manifest}" | perl -0pe "s/- name: ci-rexray-boshrelease\n  version: ['0-9a-z\+\.]+/- name: ci-rexray-boshrelease\n  version: ${rexray_release_version}/sg" > manifest.yml
 manifest="$(cat manifest.yml)"
-echo "${manifest}" | perl -0pe "s/- name: ci-scaleio-boshrelease\n  version: [0-9a-z\+\.]+/- name: ci-scaleio-boshrelease\n  version: ${scaleio_release_version}/sg" > manifest.yml
+echo "${manifest}" | perl -0pe "s/- name: ci-scaleio-boshrelease\n  version: ['0-9a-z\+\.]+/- name: ci-scaleio-boshrelease\n  version: ${scaleio_release_version}/sg" > manifest.yml
 
 bosh -n deploy
 echo "Diego has now evolved! RAWRRRR~"
